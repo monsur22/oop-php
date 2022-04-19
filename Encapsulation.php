71dbf3f035932a58e8b89deaaf44399582eba3be
monsur22 <monsurahmedshafiq@gmail.com>
2022-04-19 23:57:04 +0600

    Initial commit

---
+++ Encapsulation.php
@@ -0,0 +1,17 @@
+<?php
+
+class Class1
+{
+    function __construct()
+    {
+        $this -> name = "John";
+        echo "Class1 constructor called";
+    }
+
+    function fun1()
+    {
+        echo "Class1 fun1 called";
+    }
+}
+echo $obj->name;
+?>
